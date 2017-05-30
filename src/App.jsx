83a710d8ae93dfbac3dd83f13d4407d1676ce3ab
sevenveils <hghamilton@gmail.com>
2017-05-30 16:47:00 -0700

    App now allows user to change name and text; also, posts to chat.

--- src/App.jsx
+++ src/App.jsx
@@ -22,19 +22,30 @@ class App extends Component {
         }
       ]
     };
+    this.addNewMessage = this.addNewMessage.bind(this);
   }
 
-  componentDidMount() {
-    console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages})
-    }, 3000);
+  // componentDidMount() {
+  //   console.log("componentDidMount <App />");
+  //   setTimeout(() => {
+  //     console.log("Simulating incoming message");
+  //     // Add a new message to the list of messages in the data store
+  //     const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+  //     const messages = this.state.messages.concat(newMessage)
+  //     // Update the state of the app component.
+  //     // Calling setState will trigger a call to render() in App and all child components.
+  //     this.setState({messages: messages})
+  //   }, 3000);
+  // }
+
+  addNewMessage(name, content) {
+    const message = {
+      id: this.state.messages.length + 1,
+      username: name,
+      content
+    };
+    const newMessages = this.state.messages.concat(message);
+    this.setState({messages: newMessages});
   }
 
   render() {
@@ -44,7 +55,7 @@ class App extends Component {
             <a href='/' className='navbar-brand'>Chatty</a>
           </nav>
         <MessageList messages={this.state.messages}/>
-        <ChatBar currentUser={this.state.currentUser.name}/>
+        <ChatBar user={ this.state.currentUser.name } newMessage={ this.addNewMessage }/>
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,11 +1,49 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
+
+constructor(props) {
+  super(props);
+
+  this.state = {
+    username: this.props.user,
+    message: ''
+  }
+
+
+this.onNameChange = this.onNameChange.bind(this);
+this.onContentChange = this.onContentChange.bind(this);
+this.onKeyDown = this.onKeyDown.bind(this);
+
+}
+
+onNameChange(event) {
+  console.log(event.target.value);
+  this.setState({ username: event.target.value});
+}
+
+onContentChange(event) {
+  console.log(event.target.value);
+  this.setState({ message: event.target.value });
+}
+
+onKeyDown(event) {
+  console.log(event.charCode);
+  if (event.charCode == 13) {
+    this.props.newMessage(this.state.username, this.state.message);
+  }
+
+
+}
+
   render() {
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" value={this.props.currentUser}/>
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-username" placeholder="Your Name (Optional)" value={ this.state.username } onChange={ this.onNameChange } onKeyDown= {this.onKeyDown }/>
+        <input className="chatbar-message"
+          placeholder="Type a message and hit ENTER"
+          onChange={ this.onContentChange }
+          onKeyPress={ this.onKeyDown } />
       </footer>
     );
   }
